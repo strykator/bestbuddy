631239cd48dee0acfcb5b7e36f6f1cd1c5ab4a50
Hoang Tran <strykator@gmail.com>
2020-04-26 16:35:20 -0500

    convert javascript to typescript

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '@common/Layout'
+import React from 'react'
+import Home from './views/home/index'
+
+const App = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  )
+}
+
+export default App
